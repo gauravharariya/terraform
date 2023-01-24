2df10760365bda63a772beb9f1b4c63c53e4bf57
gauravharariya <grvharariya@gmail.com>
2023-01-24 16:22:57 +0530

    test10

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ locals {
 
 module "dev_vpc"{
   source="./modules/dev_vpc"
-  name = local.naming
+  naming = local.naming
   env= var.env  
 }
 
 module "dev_subnet"{
   source ="./modules/dev_subnet"
-  name = local.naming
+  naming = local.naming
   env = var.env
   depends_on=[module.dev_vpc]
   dev_vpc_id= module.dev_vpc.vpc_id
