bc220589fd482a28c91a7c273dec102df6eb32f4
gauravharariya <grvharariya@gmail.com>
2023-01-25 14:04:30 +0530

    test15

--- providers.tf
+++ providers.tf
@@ -4,6 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+  },
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-dataeng-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
   }
 }
 
