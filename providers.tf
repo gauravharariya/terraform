d5d6625f5772ac865660005169a5437b76e189b7
gauravharariya <grvharariya@gmail.com>
2023-01-25 14:33:40 +0530

    test16

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "terraform-dataeng-bucket"
-    key            = "global/s3/terraform.tfstate"
+    key            = "global/terraform.tfstate"
     region         = "us-east-2"
 
     # Replace this with your DynamoDB table name!
