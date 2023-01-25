4d28634df3d0d8ca807713c1b13ab608f6f48d9f
gauravharariya <grvharariya@gmail.com>
2023-01-25 16:20:30 +0530

    test28

--- providers.tf
+++ providers.tf
@@ -1,24 +1,24 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # terraform {
-#   backend "s3" {
-#     # Replace this with your bucket name!
-#     bucket         = "terraform-dataeng-bucket"
-#     key            = "global/terraform.tfstate"
-#     region         = "us-east-2"
-
-#     # Replace this with your DynamoDB table name!
-#     dynamodb_table = "terraform-up-and-running-locks"
-#     encrypt        = true
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
 #   }
 # }
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-dataeng-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
 
 provider "aws" {
   region = var.region
