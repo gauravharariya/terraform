60191b728b96e2c3ec3ae2741e0cb5bcf1fb52cb
gauravharariya <grvharariya@gmail.com>
2023-01-25 12:16:12 +0530

    test14

--- main.tf
+++ main.tf
@@ -3,36 +3,46 @@ locals {
 }
 
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform_dataeng_bucket"
+ 
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
 module "dev_vpc"{
   source="./modules/dev_vpc"
   naming = local.naming
   env= var.env  
 }
 
-module "dev_subnet"{
-  source ="./modules/dev_subnet"
-  naming = local.naming
-  env = var.env
-  depends_on=[module.dev_vpc]
-  dev_vpc_id= module.dev_vpc.vpc_id
-}
+# module "dev_subnet"{
+#   source ="./modules/dev_subnet"
+#   naming = local.naming
+#   env = var.env
+#   depends_on=[module.dev_vpc]
+#   dev_vpc_id= module.dev_vpc.vpc_id
+# }
 
-module "dev_igw"{
-  source = "./modules/dev_igw"
-  naming = local.naming
-  env = var.env
-  depends_on = [module.dev_vpc,module.dev_subnet]
-  dev_vpc_id = module.dev_vpc.vpc_id
-  dev_subnet_public_id = module.dev_subnet.dev_subnet_public_id
+# module "dev_igw"{
+#   source = "./modules/dev_igw"
+#   naming = local.naming
+#   env = var.env
+#   depends_on = [module.dev_vpc,module.dev_subnet]
+#   dev_vpc_id = module.dev_vpc.vpc_id
+#   dev_subnet_public_id = module.dev_subnet.dev_subnet_public_id
 
-}
+# }
 
-module "dev_nat" {
-  source = "./modules/dev_nat"
-  naming= local.naming
-  env=var.env
-  depends_on = [module.dev_subnet,module.dev_igw]
-  dev_subnet_public_id = module.dev_subnet.dev_subnet_public_id
-  dev_subnet_private_id = module.dev_subnet.dev_subnet_private_id
-  dev_vpc_id= module.dev_vpc.vpc_id
-}
+# module "dev_nat" {
+#   source = "./modules/dev_nat"
+#   naming= local.naming
+#   env=var.env
+#   depends_on = [module.dev_subnet,module.dev_igw]
+#   dev_subnet_public_id = module.dev_subnet.dev_subnet_public_id
+#   dev_subnet_private_id = module.dev_subnet.dev_subnet_private_id
+#   dev_vpc_id= module.dev_vpc.vpc_id
+# }
