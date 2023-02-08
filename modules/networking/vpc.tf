d5c579a70fe2817bdeeea2db0c8222370a925b86
gauravharariya <grvharariya@gmail.com>
2023-02-08 14:07:24 +0530

    folder path updated

---
+++ modules/networking/vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "vpc" {
+
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = var.vpc_dns_hostname_flag
+  enable_dns_support   = var.vpc_dns_support_flag
+
+  # cidr block iteration found in the terraform.tfvars file
+  tags = {
+    Name = var.vpc_name
+    ENV  = var.env
+  }
+}
