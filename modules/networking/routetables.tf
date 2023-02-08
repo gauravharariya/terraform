6eef02ce3aed6fcba256679977e4205ec72739a5
gauravharariya <grvharariya@gmail.com>
2023-02-08 16:32:40 +0530

    igw added

---
+++ modules/networking/routetables.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "public_rt" {
+
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    name=var.route_table_public_name,
+    env= "dev"
+  }
+
+}
+
+resource "aws_route_table_association" "public_rt_assn" {
+
+
+# Public Subnet ID
+  subnet_id      = var.public_subnet1.id
+
+#  Route Table ID
+  route_table_id = aws_route_table.public_rt.id
+}
